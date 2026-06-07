6dbda8cc65d433cecdb503f9da669bee5b3b5b69
Mredul <mredulsaha1000@gmail.com>
2026-06-07 23:15:51 +0600

    Page Count

---
+++ Basics/PageCount.cs
@@ -0,0 +1,42 @@
+// Problem: Sock Merchant
+// Platform: HackerRank
+// link: https://www.hackerrank.com/challenges/drawing-book/problem?isFullScreen=true
+
+
+class Result20
+{
+
+    /*
+     * Complete the 'pageCount' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER p
+     */
+
+    public static int pageCount(int n, int p)
+    {
+    int front = p / 2;
+    int back = n / 2 - p / 2;
+
+    return Math.Min(front, back);
+    }
+
+}
+
+class Solution22
+{
+    public static void PageCount(string[] args)
+    {
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int p = Convert.ToInt32(Console.ReadLine().Trim());
+
+        int result = Result20.pageCount(n, p);
+ 
+        Console.WriteLine(result);
+
+    }
+}
